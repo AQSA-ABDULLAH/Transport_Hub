e1c4b582c5c094248bad14daff77b62541aac436
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-26 11:37:02 +0500

    Create database Connections

--- Code/Server/index.js
+++ Code/Server/index.js
@@ -1,5 +1,9 @@
 const express = require ('express');
 const app = express();
+app.use(express.json());
+
+// For connection of DB
+require('./db/connection');
 
 app.get("/", (req,res)=>{
     res.send("Working website")
