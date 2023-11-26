2721cdf5489048d3fbbf3ffe143e8dfda17afb97
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-26 14:58:50 +0500

    complete backend

--- Code/Server/models/Transporter.js
+++ Code/Server/models/Transporter.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 
 const transporterSchema = new mongoose.Schema({
-    fullName:String,
-    phoneNo:String,
-    email: String,
-    experience:String,
-    dateOfBirth:String,
-    address:String,
-    city:String,
-    education:String
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email id already Exist"],
+        validate(value) {
+            if (!validate.isEmail(value)) {
+                throw new Error("Invalid Email");
+            }
+        }
+    },
+    phoneNo: {
+        type: Number,
+        required: true,
+    },
+    experience: String,
+    dateOfBirth: String,
+    address: String,
+    city: String,
+    education: String
 });
-module.exports = mongoose.model("transporters",transporterSchema);
+module.exports = mongoose.model("transporters", transporterSchema);
