274babd39698bffef4b04909e454900b1262cd27
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-26 13:29:46 +0500

    create SignUp Api

--- Code/Server/index.js
+++ Code/Server/index.js
@@ -2,11 +2,20 @@ const express = require ('express');
 const app = express();
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
+
+const Transporter = require('./models/Transporter');
+
 // For connection of DB
 require('./db/connection');
 
-app.get("/", (req,res)=>{
-    res.send("Working website")
+app.post("/register", async(req, res)=>{
+    let transporters = new Transporter(req.body);
+    let result = await transporters.save();
+    // result = result.toObject();
+    // delete result.password;
+    res.send(result);
 });
 
 app.listen(6000);
