d28eee56d3431d2e370f0b51538f3b36447e9d7f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-28 16:43:23 +0500

    Integrate Login API

--- Code/Client/users/src/pages/SignIn.js
+++ Code/Client/users/src/pages/SignIn.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function SignIn() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const auth = localStorage.getItem('user');
+    if(auth){
+      navigate("/")
+    }
+  })
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = { email, password };
+      const response = await axios.post('http://localhost:5000/user_signIn', formData);
+      if(response){
+        localStorage.setItem("user", JSON.stringify(response));
+        navigate("/")
+      }else{
+        alert("Plz Enter correct details")
+      }
+    } 
+    catch (error) {
+      console.error('Plz Enter correct details:', error.message, error.response);
+    }
+  };
 
   return (
     <div className='container' id="login">
-      <form>
+      <form onSubmit={handleLogin}>
         <h2 className='text-center pt-3'>SIGN IN</h2>
         <div className="mb-3">
           <label className="form-label">Email address</label>
