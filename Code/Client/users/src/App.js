0d850d5a023b3207bd76303b271a8724be873ecc
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-28 10:57:56 +0500

    Set Navigation

--- Code/Client/users/src/App.js
+++ Code/Client/users/src/App.js
@@ -4,18 +4,25 @@ import Navbar from './components/Navbar';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar title="TransportHub" services="Services" LP="Loyality Program" MS="Manage Bookings" Subscribe="Subscribe" />
+        <Navbar/>
         <Routes>
+          <Route exact path="/" element={<LandingPage/>} />
+          <Route exact path="/aboutUs" element={<h1>About Us Page</h1>} />
+          <Route exact path="/services" element={<h1>Services Page</h1>} />
+          <Route exact path="/loyaltyProgram" element={<h1>Loyalty Program</h1>} />
+          <Route exact path="/profile" element={<h1>User Profile</h1>} />
+          <Route exact path="/manageBooking" element={<h1>Manage Booking</h1>} />
+          <Route exact path="/logout" element={<h1>Logout</h1>} />
           <Route exact path="/signUp" element={<SignUp />} />
-          <Route exact path='/signIn' element={<SignIn/>} />
+          <Route exact path='/signIn' element={<SignIn />} />
         </Routes>
       </BrowserRouter>
-
     </>
   );
 }
--- Code/Client/users/src/components/Navbar.js
+++ Code/Client/users/src/components/Navbar.js
@@ -65,34 +65,24 @@
 
 
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <nav className={`header ${isOpen ? 'open' : ''}`}>
+    <nav>
       <div className='nav-container'>
-        <Link className="logo text-white font-italic" to="/">FoodFrenzy</Link>
-        <div className='menu-icon ' onClick={toggleNavbar}>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-        </div>
-
-        <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-          <li><Link to="/signUp">Signup</Link></li>
-          <li><Link to="/signIn">Login</Link></li>
-
-       
+        <h1><Link className="logo font-italic" to="/">FoodFrenzy</Link></h1>
+        <ul>
+          <li><Link to="/aboutUs">About Us</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/loyaltyProgram">Loyalty Program</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/manageBooking">Manage Booking</Link></li>
+          <li><Link to="/logout">Logout</Link></li>
         </ul>
+        <button><Link to="/signUp">Signup</Link></button>
+        <button><Link to="/signIn">Login</Link></button>
       </div>
     </nav>
   )
