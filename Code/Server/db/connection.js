a887e194a0d1eb725f6e6b967d745b4a36175063
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-29 17:49:58 +0500

    Hide Database URL

--- Code/Server/db/connection.js
+++ Code/Server/db/connection.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://aqsaabdullah38403:WfvspNzsKhMgFGIK@cluster0.lvmi7hp.mongodb.net/Transport_Hub');
-console.log("Connection Successfull");
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection Successful");
+}).catch((error) => {
+    console.error("Error occurred", error);
+});
