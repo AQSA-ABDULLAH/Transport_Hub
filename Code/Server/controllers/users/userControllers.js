464809298e249e47a59188b8b7ab165143f090a0
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-11-29 22:46:39 +0500

    create controller

--- Code/Server/controllers/users/userControllers.js
+++ Code/Server/controllers/users/userControllers.js
@@ -1,13 +1,31 @@
-import UserModel from "../../models/Users.js";
+
+const User = require("../../models/Users");
 
 class UserController {
+    static userRegistration = async (req, res) => {
+        try {
+            const { fullName, email, phoneNumber, password, city, address } = req.body;
 
+            if (!fullName || !email || !phoneNumber || !password || !city || !address) {
+                return res.status(422).json({ error: "Please fill in all fields properly" });
+            }
+
+            const userExist = await User.findOne({ email: email });
+
+            if (userExist) {
+                return res.status(422).json({ error: "User already exists" });
+            }
+
+            const user = new User({ fullName, email, phoneNumber, password, city, address });
+
+            await user.save();
+
+            res.status(201).json({ message: "User registered successfully" });
+        } catch (error) {
+            console.error("Error in user registration:", error);
+            res.status(500).json({ error: "Failed to register" });
+        }
+    }
 }
 
-app.post("/user_register", async(req, res)=>{
-    let user = new Users(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password;
-    res.send(result);
-});
+module.exports = { UserController };
--- Code/Server/routes/user.js
+++ Code/Server/routes/user.js
@@ -1,36 +1,6 @@
 const express = require('express');
+const { UserController } = require('../controllers/users/userControllers'); // Change the import to use named import
 const router = express.Router();
 
-require('../db/connection');
-const User = require("../models/Users");
-
-router.post('/user_signUp', (req, res) => {
-    const { fullName, email, phoneNumber, password, city, address } = req.body;
-
-    if (!fullName || !email || !phoneNumber || !password || !city || !address) {
-        return res.status(422).json({ error: "Please fill in all fields properly" });
-    }
-
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(422).json({ error: "User already exists" });
-            }
-
-            const user = new User({ fullName, email, phoneNumber, password, city, address });
-
-            user.save()
-                .then(() => {
-                    res.status(201).json({ message: "User registered successfully" });
-                })
-                .catch((error) => {
-                    res.status(500).json({ error: "Failed to register" });
-                    console.log(error);
-                });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-
+router.post("/user_signUp", UserController.userRegistration);
 module.exports = router;
