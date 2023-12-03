121a26f9dd171c7e76379ef7d02fc7908f54e778
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-12-03 15:24:18 +0500

    Integrate Signup API

--- Code/Client/users/src/pages/SignUp.js
+++ Code/Client/users/src/pages/SignUp.js
@@ -4,34 +4,44 @@ import axios from 'axios';
 
 export default function SignUp() {
   const [fullName, setFullName] = useState('');
-  const [phoneNo, setPhoneNo] = useState('');
+  const [phoneNumber, setphoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [address, setAddress] = useState('');
   const [city, setCity] = useState('');
   const navigate = useNavigate();
 
-    useEffect(()=>{
-      const auth = localStorage.getItem('user');
-      if(auth){
-        navigate("/")
-      }
-    })
+  useEffect(() => {
+    const auth = localStorage.getItem('user');
+    if (auth) {
+      navigate("/")
+    }
+  })
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const formData = { fullName, phoneNo, email, password, address, city };
-      const response = await axios.post('"http://localhost:5000/api/user/register"', formData);
+      const formData = { fullName, email, phoneNumber, password, city, address };
+      const response = await axios.post("http://localhost:5000/api/user/user_signUp", formData);
+
+      // Assuming the server returns a success message in the response
+      console.log('Data submitted successfully:', response.data.message);
+
+      // You can also display a success message to the user
+      alert('Data submitted successfully!');
 
       localStorage.setItem("user", JSON.stringify(response));
-      navigate("/")
-    } 
+      navigate("/");
+    }
     catch (error) {
       console.error('Error submitting form:', error.message, error.response);
+
+      // You can display an error message to the user
+      alert('Error submitting form. Please try again.');
     }
   };
+
 
   return (
     <div>
@@ -52,8 +62,8 @@ export default function SignUp() {
           Phone No.:
           <input
             type="tel"
-            value={phoneNo}
-            onChange={(e) => setPhoneNo(e.target.value)}
+            value={phoneNumber}
+            onChange={(e) => setphoneNumber(e.target.value)}
             required
           />
         </label>
