14a43c61038952b169e5661b90fc04ed28442826
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-12-05 21:53:03 +0500

    create website content tab

--- Code/Client/admin/src/pages/Websitecontent/tabs/about/AboutTab.js
+++ Code/Client/admin/src/pages/Websitecontent/tabs/about/AboutTab.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Aboutus from "../../../components/molecules/aboutus/Aboutus";
+import Aboutus from "../../../../components/molecules/aboutus/Aboutus"
 import style from "../about/aboutTab.module.css";
 
 const AboutTab = () => {
