2d28175b25921f82f17e106caa5892bf9ebb3b1f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-12-06 19:37:30 +0500

    create car model

---
+++ Code/Server/models/Cars.js
@@ -0,0 +1,80 @@
+const carSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    trim: true
+  },
+  nameAndModel: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  seats: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  transmission: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  bags: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  mileLimit: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  color: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  fuelType: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  engineType: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  zone: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  discount: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  startDate: {
+    type: Date,
+    required: true,
+    trim: true
+  },
+  endDate: {
+    type: Date,
+    trim: true
+  },
+},
+  {
+    timestamps: true
+  });
+const Car = mongoose.model('Car', carSchema);
+export default Car;
