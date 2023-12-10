70dcf042569425fa8b9cf71be56172ade3764c53
AneeqaWaheed <aneeqawaheed19@gmail.com>
2023-12-10 16:47:13 +0500

    image handled with both cars and trips,  dependencies required are joi and multer

--- Code/Server/controllers/transportManagment/carsController.js
+++ Code/Server/controllers/transportManagment/carsController.js
@@ -1,32 +1,39 @@
 const mongoose = require("mongoose");
 const Cars = require("../../models/Cars");
-
+const Joi = require("joi");
 class CarsController {
+    static CarPackageValidationSchema = Joi.object({
+        nameAndModel: Joi.string().required(),
+        type: Joi.string().required(),
+        description: Joi.string().required(),
+        transmission: Joi.string().required(),
+        color: Joi.string().required(),
+        fuelType: Joi.string().required(),
+        engineType: Joi.string().required(),
+        image: Joi.string().trim().required(),
+        bags: Joi.number().required(),
+        mileLimit: Joi.number().required(),
+        discount: Joi.number().required(),
+        price: Joi.date(),
+        price: Joi.date(),
+        
+      });
+    
     static addCars = async (req, res) => {
-        try {
-            const {
-                image, nameAndModel, type, seats, transmission, bags, mileLimit,
-                price, color, fuelType, engineType, zone, discount, startDate, endDate
-            } = req.body;
-
-            if (!image || !nameAndModel || !type || !seats || !transmission ||
-                !bags || !mileLimit || !price || !color || !fuelType ||
-                !engineType || !zone || !discount || !startDate || !endDate) {
-                return res.status(400).send({ status: "failed", message: "Incomplete data provided" });
-            }
-
-            const newCar = new Cars({
-                image, nameAndModel, type, seats, transmission, bags, mileLimit,
-                price, color, fuelType, engineType, zone, discount, startDate, endDate
-            });
-
-            await newCar.save();
-
-            res.status(201).send({ status: "success", message: "Car saved successfully", data: newCar });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send({ status: "failed", message: "Internal Server Error" });
-        }
+       
+            const data = req.body;
+            data.image = req?.file?.filename;
+            const { error } = this.CarPackageValidationSchema.validate(data);
+            try {
+                const newCar = await Cars.create(data);
+                console.log("Data saved");
+                res.status(201).send({ status: "success", message: "Trip saved successfully", data: newCar });
+              } catch (error) {
+                console.error(error); // Log the error
+                res.status(500).send({ status: "failed", message: "Internal Server Error" });
+              }
+            
+        
     };
 }
 
