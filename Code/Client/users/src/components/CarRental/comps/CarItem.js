0374bf39281270a41f0ca00888e56c5b76eede0b
AneeqaWaheed <aneeqawaheed19@gmail.com>
2023-12-10 17:12:14 +0500

    CarItem

--- Code/Client/users/src/components/CarRental/comps/CarItem.js
+++ Code/Client/users/src/components/CarRental/comps/CarItem.js
@@ -1,16 +1,32 @@
-// CarItem.js
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 
-const CarItem = ({ car }) => {
+const CarItem = () => {
   return (
-    <div className="card">
-      <img src={car.imageUrl} className="card-img-top" alt={`Car ${car.id}`} />
-      <div className="card-body">
-        <h5 className="card-title">{car.name}</h5>
-        <p className="card-text">Location: {car.location}</p>
-      </div>
-    </div>
+    <Card style={{ width: "18rem", margin: "20px" }}>
+      <Card.Body>
+        <Card.Title>Toyota Camry</Card.Title>
+        <Card.Text>
+          The Toyota Camry is a reliable and comfortable sedan, perfect for your
+          road trips and daily commute.
+        </Card.Text>
+        <Card.Img variant="top" src="car1.png" alt="Car Image" />
+        <div className="mb-3">
+          <h6>Upgrades:</h6>
+          <div className="d-flex align-items-center">
+            <div className="me-2">🚗</div> Automatic Transmission
+          </div>
+          <div className="d-flex align-items-center">
+            <div className="me-2">🔊</div> Premium Sound System
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center">
+          <Button variant="success">Rent Now</Button>
+          <div className="text-muted">$50 per day</div>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
-
 export default CarItem;
