5d34bd611e5718149032d93858e1c768d842363b
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-12-13 18:28:46 +0500

    style customer notice

--- Code/Client/users/src/components/atoms/CustomersNotice/BookingFormNotice.js
+++ Code/Client/users/src/components/atoms/CustomersNotice/BookingFormNotice.js
@@ -5,7 +5,7 @@ export default function () {
   return (
     <>
         <div className={style.noticeContainer}>
-            <div className={style.button}><button>Note</button></div>
+            <div><button className={style.button}>Note</button></div>
             <div className={style.text}>Please enter correct details for your booking. These details are necessary to verify your identity.</div>
         </div>
     </>
--- Code/Client/users/src/pages/CarRental/bookingPage/RentalBookingPage.js
+++ Code/Client/users/src/pages/CarRental/bookingPage/RentalBookingPage.js
@@ -3,21 +3,27 @@ import Navbar from "../../../components/atoms/Navbar/Navbar";
 import RentalPriceCard from '../../../components/molecules/carRental/priceCard/RentalPriceCard';
 import style from "./rentalBookingPage.module.css";
 import BookingFormNotice from '../../../components/atoms/CustomersNotice/BookingFormNotice';
+import RentalBookingForm from "../../../components/sections/carRentalSections/BookingSection/RentalBookingForm"
 
 
 export default function RentalBookingPage() {
   return (
     <>
-            <Navbar />
-            <div className={style.container}>
-                <div className={style.bookingContainer}>
-                  <div className={style.Notice}><BookingFormNotice/></div>
-                </div>
-                <div className={style.priceContainer}>
-                    <RentalPriceCard />
-                </div>
-            </div>
-        </>
+      <Navbar />
+      <div className={style.container}>
+        <div className={style.bookingContainer}>
+          <div className={style.Notice}>
+            <BookingFormNotice />
+          </div>
+          <div className={style.Booking}>
+            <RentalBookingForm/>
+          </div>
+        </div>
+        <div className={style.priceContainer}>
+          <RentalPriceCard />
+        </div>
+      </div>
+    </>
   )
 }
 
