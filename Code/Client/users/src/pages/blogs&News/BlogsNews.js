80ba6c98ba25b8f0cd18d558e90cacfa4f92d08b
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-01-03 14:29:56 +0500

    create blog page

--- Code/Client/users/src/pages/blogs&News/BlogsNews.js
+++ Code/Client/users/src/pages/blogs&News/BlogsNews.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Navbar from '../../components/atoms/Navbar/Navbar';
-import style from './blogsNews.module.css'
+import style from './blogsNews.module.css';
 
 export default function BlogsNews() {
   return (
     <>
-    <Navbar/>
-    <div>
-        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png"/>
-    </div>
+      <Navbar />
+      <div>
+        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png" alt="Blog Cover" />
+        <div className={style.flexContainer}>
+          <section className={style.flexSections}>
+            <h4>first</h4>
+          </section>
+          <section>
+            <h4>second</h4>
+          </section>
+        </div>
+      </div>
     </>
-  )
+  );
 }
+
