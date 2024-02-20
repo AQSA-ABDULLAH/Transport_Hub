0ba11a6535495511a19ce523dd6cd81f372a2e80
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-02-20 23:26:47 +0500

    create user model

--- Code/Server/models/Users.js
+++ Code/Server/models/Users.js
@@ -2,45 +2,16 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
 
 const usersSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    address: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    city: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    is_admin: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    is_verified: {
-        type: Boolean,
-        default: false
-    },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+    phoneNumber: { type: String, required: true, trim: true },
+    password: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    zipCode: {type: Number, required: true, trim: true},
+    address: { type: String, required: true, trim: true },
+    is_admin: { type: Boolean, default: false, required: true },
+    is_verified: { type: Boolean, default: false },
     tokens: [
         {
             token: {
