6d4a4ed1303822187a0c99803ef7e98414338524
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-02-23 22:48:29 +0500

    resolve authentication issues

--- Code/Client/users/src/components/molecules/Form.js
+++ Code/Client/users/src/components/molecules/Form.js
@@ -55,7 +55,6 @@ const Form = () => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form submitted");
     // Check if password and confirmPassword match
     if (formData.password !== formData.confirmPassword) {
       setError("Passwords do not match");
@@ -64,8 +63,7 @@ const Form = () => {
     console.log("collected data", formData, "length is", error.length);
 
     if (error.length === 0) {
-      axios
-        .post("http://localhost:8000/api/user/register", formData)
+      axios.post("http://localhost:5000/api/user/user_signUp", formData)
         .then((response) => {
           // Handle the response if needed
           console.log("API response:", response.data);
@@ -75,98 +73,98 @@ const Form = () => {
           console.error("Error:", error);
         });
     }
+  }
+    return (
+      <form action="" className={styles.form}>
+        <div className={styles.formRow}>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+          />
+        </div>
+
+        <div className={styles.formRow}>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+          />
+          <input
+            onChange={handleChange}
+            type="tel"
+            name="phoneNumber"
+            id="phoneNumber"
+            placeholder="Phone Number"
+          />
+        </div>
+
+        <div className={styles.formRow}>
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="confirmPassword"
+            id="confirmPasswor"
+            placeholder="Confirm Password"
+          />
+        </div>
+
+        <div className={styles.formRow}>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City"
+          />
+          <input
+            onChange={handleChange}
+            type="text"  // Corrected type from "zipCode" to "text"
+            name="zipCode"
+            id="zipCode"
+            placeholder="Zip Code"
+          />
+
+        </div>
+
+        <div className={styles.formRow}>
+          <textarea
+            onChange={handleChange}
+            type="text"
+            name="address"
+            id="address"
+            placeholder="Street Address"
+          />
+        </div>
+
+        <div className={styles.btns}>
+          <Button
+            btnText="Join"
+            primary
+            width={"20%"}
+            btnClick={handleSubmit}
+          />
+        </div>
+      </form>
+    );
   };
-  return (
-    <form action="" className={styles.form}>
-      <div className={styles.formRow}>
-        <input
-          onChange={handleChange}
-          type="text"
-          name="firstName"
-          id="firstName"
-          placeholder="First Name"
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          name="lastName"
-          id="lastName"
-          placeholder="Last Name"
-        />
-      </div>
 
-      <div className={styles.formRow}>
-        <input
-          onChange={handleChange}
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email Address"
-        />
-        <input
-          onChange={handleChange}
-          type="tel"
-          name="phoneNumber"
-          id="phoneNumber"
-          placeholder="Phone Number"
-        />
-      </div>
-
-      <div className={styles.formRow}>
-        <input
-          onChange={handleChange}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-        />
-        <input
-          onChange={handleChange}
-          type="password"
-          name="confirmPassword"
-          id="confirmPasswor"
-          placeholder="Confirm Password"
-        />
-      </div>
-
-      <div className={styles.formRow}>
-        <input
-          onChange={handleChange}
-          type="text"
-          name="city"
-          id="city"
-          placeholder="City"
-        />
-        <input
-          onChange={handleChange}
-          type="text"  // Corrected type from "zipCode" to "text"
-          name="zipCode"
-          id="zipCode"
-          placeholder="Zip Code"
-        />
-
-      </div>
-
-      <div className={styles.formRow}>
-        <textarea
-          onChange={handleChange}
-          type="text"
-          name="address"
-          id="address"
-          placeholder="Street Address"
-        />
-      </div>
-
-      <div className={styles.btns}>
-        <Button
-          btnText="Join"
-          primary
-          width={"20%"}
-          btnClick={handleSubmit}
-        />
-      </div>
-    </form>
-  );
-};
-
-export default Form;
+  export default Form;
