bff66140462bf655db7ddd3e8c1d0428c15a05de
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-08 09:54:02 +0500

    create header

---
+++ Code/Client/users/src/pages/blogs&News/BlogsNews.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './blogsNews.module.css'
+
+export default function BlogsNews() {
+  return (
+    <>
+    <div>
+        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png"/>
+    </div>
+    </>
+  )
+}
