be682b8ec19230e56dbdf7a35d9bd8f02901bad4
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-08 15:26:41 +0500

    create blog filter

--- Code/Client/users/src/components/sections/blog&new/blog&new1section/Blogs.js
+++ Code/Client/users/src/components/sections/blog&new/blog&new1section/Blogs.js
@@ -4,36 +4,22 @@ import style from './blogs.module.css'
 export default function Blogsection1molecule() {
     return (
         <>
-            <div className={style.Container}>
                 <div className={style.cards}>
                     <div className={style.blogCard}>
-                        <img src="" alt="blogImage" />
                         <div>
-                            <p>Engineering, Backend, web</p>
-                            <h2>Building Scalable, Real-Time Chat to Improve Customer Experience</h2>
-                            <p>20 February / Global</p>
+                            <img className={style.blogImage} src="/assets/images/blogs&News/blogImage6.jpg" alt="blogImage" />
+                        </div>
+                        <div className={style.blog}>
+                            <h2>2023 Recap: fostering community and championing sustainability</h2>
+                            <p>In 2023, Rome2Rio lived our social impact value to its fullest, driving positive change in our 
+                                local community and lending a hand to groups and individuals who need it 
+                                most. Rome2Rio offers 2 days paid volunteer days each year. This…</p>
+                            <p>2Posted December 20, 2023       Read more ›</p>
                         </div>
                     </div>
 
-                    <div className={style.blogCard}>
-                        <img src="" alt="blogImage" />
-                        <div>
-                            <p>Engineering, Backend, web</p>
-                            <h2>Building Scalable, Real-Time Chat to Improve Customer Experience</h2>
-                            <p>20 February / Global</p>
-                        </div>
-                    </div>
+    
 
-                    <div className={style.blogCard}>
-                        <img src="" alt="blogImage" />
-                        <div>
-                            <p>Engineering, Backend, web</p>
-                            <h2>Building Scalable, Real-Time Chat to Improve Customer Experience</h2>
-                            <p>20 February / Global</p>
-                        </div>
-                    </div>
-                
-                </div>
             </div>
         </>
     )
--- Code/Client/users/src/components/sections/blog&new/blog&new2section/Blogfilter.js
+++ Code/Client/users/src/components/sections/blog&new/blog&new2section/Blogfilter.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import style from './blogfilter.module.css'
+import style from './blogfilter.module.css';
+import { FaSearch } from 'react-icons/fa'; // Import the search icon from react-icons
 
 export default function Blogsection2molecule() {
     return (
-        <>
         <div className={style.blog_section}>
-            <div className={style.recent_post}>cscs</div>
-            <div className={style.blog_subscribe}>cdsc</div>
-            <div className={style.blog_category}>fefde</div>
-            <div className={style.latest_tweet}>fdgrfeg</div>
+            <div className={style.search_container}>
+                <input type="text" placeholder="Search..." className={style.search_input} />
+                <button className={style.search_button}>
+                    <FaSearch />
+                </button>
+            </div>
+
+            <div className={style.blog_subscribe}>
+                <h2>Join our newsletter</h2>
+                <p>Get regular access to our best deals and tips in your inbox.</p>
+                <div className={style.subscribe_input}>
+                    <input type="text" placeholder="Search..." />
+                    <button>Subscribe</button>
+                </div>
+                <p>Rome2Rio will use the information you provide on this form to provide product 
+                    updates and marketing offers. You can change your mind at any time by clicking 
+                    the unsubscribe link in the footer of any email you receive from us.</p>
+            </div>
+            <div className={style.blog_category}>
+                <h2>Categories</h2>
+                <p>People & Culture</p>
+                <p>Career</p>
+                <p>Travel Technology</p>
+                <p>Other</p>
+            </div>
+            <div className={style.latest_tweet}>
+                <h2>Latest Tweet</h2>
+            </div>
         </div>
-        </>
-    )
+    );
 }
+
