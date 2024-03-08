47e78044e5974030e1e8f004604e4328b07d7363
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-08 10:49:28 +0500

    blogsection

---
+++ Code/Client/users/src/components/molecules/blog&newSection/blogsection2molecules/Blogsection2molecule.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './blogsection2molecule.module.css'
+
+export default function Blogsection2molecule() {
+    return (
+        <>
+        <div className={style.blog_section}>
+            <div className={style.recent_post}>cscs</div>
+            <div className={style.blog_subscribe}>cdsc</div>
+            <div className={style.blog_category}>fefde</div>
+            <div className={style.latest_tweet}>fdgrfeg</div>
+        </div>
+        </>
+    )
+}
--- Code/Client/users/src/pages/blogs&News/BlogsNews.js
+++ Code/Client/users/src/pages/blogs&News/BlogsNews.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import style from './blogsNews.module.css'
+import Blogsection1molecule from '../../components/molecules/blog&newSection/blogsection1molecules/Blogsection1molecule'
+import Blogsection2molecule from '../../components/molecules/blog&newSection/blogsection2molecules/Blogsection2molecule'
 
 export default function BlogsNews() {
   return (
     <>
-    <div>
-        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png"/>
-    </div>
+      <div>
+        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png" />
+      </div>
+      <div className={style.blogSection}>
+        <Blogsection1molecule />
+        <Blogsection2molecule />
+      </div>
     </>
   )
 }
