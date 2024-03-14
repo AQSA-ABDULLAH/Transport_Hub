b7ae9b931d6ddd7fe3786b7eacd209d6139bef87
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-14 14:02:27 +0500

    Resolve backend error

--- Code/Server/routes/blogsRoutes.js
+++ Code/Server/routes/blogsRoutes.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const BlogController = require("../controllers/blogs&news/blogController");
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+const BlogController = require("../controllers/blogs&news/blogController");
+
+const storage = multer.diskStorage({
+    destination: (cb) => {
+        cb(null)
+    },
+    filename: (file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+  });
+
+  const upload = multer({ storage: storage })
 
 //PROTECTED ROUTES
-router.post("/create-blog", BlogController.createBlog);
+router.post("/create-blog", upload.single('carImage'), BlogController.createBlog);
 router.get("/get-blog/:id", BlogController.getBlog);
 
 //PUBLIC ROUTES
