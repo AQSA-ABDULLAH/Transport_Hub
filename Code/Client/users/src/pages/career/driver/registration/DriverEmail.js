f6aa411bf2fe453f9c1b6fc8a78d1011740ab369
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-15 12:24:09 +0500

    create side section

--- Code/Client/users/src/pages/career/driver/registration/DriverEmail.js
+++ Code/Client/users/src/pages/career/driver/registration/DriverEmail.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import SideSection from '../../../../components/sections/career/sidesection/SideSection';
+import styles from "./drivermail.module.css";
 
 export default function DriverEmail() {
   return (
-    <div>DriverEmail</div>
-  )
+    <>
+      <section className={styles.container}>
+        <div className={styles.sideSection}>
+          <SideSection />
+        </div>
+        <div className={styles.mainSection}>MAIN SECTION</div>
+      </section>
+    </>
+  );
 }
+
