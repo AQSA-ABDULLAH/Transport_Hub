47c4075e13ce3af6227d0e5e9c9583c59703c96a
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-15 19:13:10 +0500

    create email page

--- Code/Client/users/src/pages/career/driver/registration/DriverEmail.js
+++ Code/Client/users/src/pages/career/driver/registration/DriverEmail.js
@@ -9,7 +9,43 @@ export default function DriverEmail() {
         <div className={styles.sideSection}>
           <SideSection />
         </div>
-        <div className={styles.mainSection}>MAIN SECTION</div>
+
+        <div className={styles.mainSection}>
+          <div className={styles.language}>
+            <p>English (United Kingdom)</p>
+          </div>
+
+          <section className={styles.form_container}>
+
+            <div className={styles.heading}>
+              <h2>
+                Create your Driver Account
+              </h2>
+              <p>
+                Then start your driving career!
+              </p>
+            </div>
+
+            <div>
+              <form>
+                <input type="email" />
+                <button>Send OTP</button>
+              </form>
+              <div><spam>OR</spam></div>
+              <div>
+                <button>
+                  <img src='./assets/images/career/google_icon.png' alt="" />
+                  Continue with Google
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <p>By proceeding, you consent to get Mail, including by automated means,
+                from Transport Hub and its affiliates to the email provided.</p>
+            </div>
+          </section>
+        </div>
       </section>
     </>
   );
