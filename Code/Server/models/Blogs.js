393dba3c6f2fd59b60a0f6c9b77d1f88fa339108
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-18 21:11:37 +0500

    update blog model

--- Code/Server/models/Blogs.js
+++ Code/Server/models/Blogs.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const blogsSchema = new mongoose.Schema({
     image: { type: String, trim: true },
     heading: { type: String, trim: true },
+    category: {type: String, trim: true},
+    writtenby: {type: String, trim: true},
     content: { type: String, trim: true },
 }, {
     timestamps: true
