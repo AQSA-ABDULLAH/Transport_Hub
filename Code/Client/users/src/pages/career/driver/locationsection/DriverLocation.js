180014be9112a64ad9ff7e0fd25c4fc94c4e5fcf
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-19 23:43:32 +0500

    create location form

--- Code/Client/users/src/pages/career/driver/locationsection/DriverLocation.js
+++ Code/Client/users/src/pages/career/driver/locationsection/DriverLocation.js
@@ -12,15 +12,17 @@ export default function DriverLocation() {
                 <div className={careerStyles.subContainer}>
                     <MediumHeader />
                     <div className={styles.location}>
-                        <h2>Earn with Uber</h2>
+                        <h2>Earn with Transport Hub</h2>
                         <p>Decide when, where, and how you want to earn.</p>
                         <form className={`${careerStyles.input_field} ${styles.inputForm}`}>
                             <h4>Where would you like to earn?</h4>
-                            <input type='text' />
+                            <input type='text' placeholder='City'/>
                         </form>
-                        <span>By proceeding, I agree that Uber or its representatives may contact me by email,
-                            phone, or SMS (including by automatic telephone dialing system) at the email address
-                            or number I provide, including for marketing purposes.</span>
+                        <div className={styles.notice}>
+                            <h4>By proceeding, I agree that Transport Hub or its representatives may contact me by email 
+                                or SMS (including by automatic telephone dialing system) at the email address
+                                or number I provide, including for marketing purposes.</h4>
+                        </div>
 
                         <div className={`${careerStyles.button} ${styles.formbutton}`}>
                             <button><FontAwesomeIcon icon={faArrowLeft} /> BACK</button>
