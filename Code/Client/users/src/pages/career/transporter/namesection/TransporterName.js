e9519d454511170ded48e7cdabdff2fb9c7c78c8
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-19 14:01:32 +0500

    create name section

--- Code/Client/users/src/pages/career/transporter/namesection/TransporterName.js
+++ Code/Client/users/src/pages/career/transporter/namesection/TransporterName.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import styles from './transportername.module.css';
+import transporterStyles from '../transporter.module.css';
 import MediumHeader from '../../../../components/sections/header-medium/MediumHeader';
 
 export default function TransporterName() {
     return (
         <>
-        <MediumHeader/>
-            <section className={styles.name_container}>
-                <h2>What's your name?</h2>
-                <p>Let us know how to properly address you</p>
-
-                <form className={styles.inputForm}>
+            <MediumHeader />
+            <section className={styles.container}>
+                <div className={styles.formdiv}>
+                    <h2>What's your name?</h2>
+                    <p>Let us know how to properly address you</p>
+            
+                <form className={`${transporterStyles.input_field} ${styles.inputForm}`}>
                     <input type='text' placeholder='Enter first name' />
                     <input type='text' placeholder='Enter last name' />
 
-                <div className={styles.button}>
-                    <button>NEXT</button>
-                    <button>BACK</button>
-                </div>
+                    <div className={`${transporterStyles.button} ${styles.formbutton}`}>
+                        <button>BACK</button>
+                        <button>NEXT</button>
+                    </div>
                 </form>
+                </div>
             </section>
         </>
     )
 }
+
+
