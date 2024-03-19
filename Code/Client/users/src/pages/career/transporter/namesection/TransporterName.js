bd1bddcfbc63d423a2e71a7fbcbc026942daeb0c
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-19 21:48:43 +0500

    style name sections

--- Code/Client/users/src/pages/career/transporter/namesection/TransporterName.js
+++ Code/Client/users/src/pages/career/transporter/namesection/TransporterName.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './transportername.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'; // Import both icons here
 import transporterStyles from '../transporter.module.css';
 import MediumHeader from '../../../../components/sections/header-medium/MediumHeader';
 
@@ -11,20 +13,21 @@ export default function TransporterName() {
                 <div className={styles.formdiv}>
                     <h2>What's your name?</h2>
                     <p>Let us know how to properly address you</p>
-            
-                <form className={`${transporterStyles.input_field} ${styles.inputForm}`}>
-                    <input type='text' placeholder='Enter first name' />
-                    <input type='text' placeholder='Enter last name' />
 
-                    <div className={`${transporterStyles.button} ${styles.formbutton}`}>
-                        <button>BACK</button>
-                        <button>NEXT</button>
-                    </div>
-                </form>
+                    <form className={`${transporterStyles.input_field} ${styles.inputForm}`}>
+                        <input type='text' placeholder='Enter first name' />
+                        <input type='text' placeholder='Enter last name' />
+
+                        <div className={`${transporterStyles.button} ${styles.formbutton}`}>
+                            <button><FontAwesomeIcon icon={faArrowLeft} /> BACK</button>
+                            <button>NEXT <FontAwesomeIcon icon={faArrowRight} /></button>
+                        </div>
+                    </form>
                 </div>
             </section>
         </>
     )
 }
+
 
 
