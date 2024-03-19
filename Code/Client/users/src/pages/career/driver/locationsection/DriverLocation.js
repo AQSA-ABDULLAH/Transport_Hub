6296d1f9055bd81e8191f216ca00635db8c3c040
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-19 23:24:24 +0500

    create location page

--- Code/Client/users/src/pages/career/driver/locationsection/DriverLocation.js
+++ Code/Client/users/src/pages/career/driver/locationsection/DriverLocation.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import careerStyles from '../../careerpage.module.css';
+import styles from './driverlocation.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import MediumHeader from '../../../../components/sections/header-medium/MediumHeader';
 
 export default function DriverLocation() {
@@ -8,8 +11,23 @@ export default function DriverLocation() {
             <div className={careerStyles.container}>
                 <div className={careerStyles.subContainer}>
                     <MediumHeader />
-                </div>
+                    <div className={styles.location}>
+                        <h2>Earn with Uber</h2>
+                        <p>Decide when, where, and how you want to earn.</p>
+                        <form className={`${careerStyles.input_field} ${styles.inputForm}`}>
+                            <h4>Where would you like to earn?</h4>
+                            <input type='text' />
+                        </form>
+                        <span>By proceeding, I agree that Uber or its representatives may contact me by email,
+                            phone, or SMS (including by automatic telephone dialing system) at the email address
+                            or number I provide, including for marketing purposes.</span>
 
+                        <div className={`${careerStyles.button} ${styles.formbutton}`}>
+                            <button><FontAwesomeIcon icon={faArrowLeft} /> BACK</button>
+                            <button>NEXT <FontAwesomeIcon icon={faArrowRight} /></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- Code/Client/users/src/pages/career/driver/namesection/DriverName.js
+++ Code/Client/users/src/pages/career/driver/namesection/DriverName.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './drivername.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'; // Import both icons here
-import transporterStyles from '../../careerpage.module.css';
+import careerStyles from '../../careerpage.module.css';
 import MediumHeader from '../../../../components/sections/header-medium/MediumHeader';
 
 export default function DriverName() {
@@ -14,11 +14,11 @@ export default function DriverName() {
                     <h2>What's your name?</h2>
                     <p>Let us know how to properly address you</p>
 
-                    <form className={`${transporterStyles.input_field} ${styles.inputForm}`}>
+                    <form className={`${careerStyles.input_field} ${styles.inputForm}`}>
                         <input type='text' placeholder='Enter first name' />
                         <input type='text' placeholder='Enter last name' />
 
-                        <div className={`${transporterStyles.button} ${styles.formbutton}`}>
+                        <div className={`${careerStyles.button} ${styles.formbutton}`}>
                             <button><FontAwesomeIcon icon={faArrowLeft} /> BACK</button>
                             <button>NEXT <FontAwesomeIcon icon={faArrowRight} /></button>
                         </div>
