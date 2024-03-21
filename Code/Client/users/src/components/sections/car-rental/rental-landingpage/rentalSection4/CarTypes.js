333a62c826f4ec7e4ec39de187414f69750e7682
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-21 20:50:25 +0500

    create car types

--- Code/Client/users/src/components/sections/car-rental/rental-landingpage/rentalSection4/CarTypes.js
+++ Code/Client/users/src/components/sections/car-rental/rental-landingpage/rentalSection4/CarTypes.js
@@ -3,17 +3,46 @@ import CustomCarousel from "./CustomCarousel";
 
 const CarTypes = () => {
   const items = [
-    {
+   {
       id: 1,
-      title: "Item 1",
-      imageUrl: "./assets/images/item1.jpg",
+      title: "SUVs",
+      imageUrl: "./assets/images/cars/SUVs.png",
     },
     {
       id: 2,
-      title: "Item 2",
-      imageUrl: "./assets/images/item2.jpg",
+      title: "Pickup Truks",
+      imageUrl: "./assets/images/cars/PickupTrucks.PNG",
     },
-    // Add more items as needed
+    {
+      id: 3,
+      title: "Passenger Van",
+      imageUrl: "./assets/images/cars/PassengerVan.PNG",
+    },
+    {
+      id: 4,
+      title: "Guarented Models",
+      imageUrl: "./assets/images/cars/GuarentedModels.PNG",
+    },
+    {
+      id: 5,
+      title: "Mini Vans",
+      imageUrl: "./assets/images/cars/Minivans.PNG",
+    },
+    {
+      id: 6,
+      title: "Four Wheel Drive",
+      imageUrl: "./assets/images/cars/FourWheelDrive.PNG",
+    },
+    {
+      id: 7,
+      title: "Electric Cars",
+      imageUrl: "./assets/images/cars/ElectricCars.PNG",
+    },
+    {
+      id: 8,
+      title: "Convertibles",
+      imageUrl: "./assets/images/cars/CONVERTIBLES.png",
+    },
   ];
 
   return (
