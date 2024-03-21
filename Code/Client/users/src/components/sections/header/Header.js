860354ea6805fa73ebdd2dcfa7234ebb9554f2fb
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-21 13:36:50 +0500

    style navbar

--- Code/Client/users/src/components/sections/header/Header.js
+++ Code/Client/users/src/components/sections/header/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 import Button from "../../atoms/button/Button";
-import { FaBars, FaUser } from "react-icons/fa";
+import { FaBars, FaUser, FaAngleDown } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
 import useUserAction from "../../../utils/customHooks/useUserAction";
@@ -10,13 +10,14 @@ import useUserAction from "../../../utils/customHooks/useUserAction";
 const Navbar = () => {
   const navigate = useNavigate();
   const [Mobile, setMobile] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false); 
   const { reduxState, dispatch, actions } = useUserAction();
 
   return (
     <>
       <nav className={styles.navbar}>
 
-        <div>
+        <div className={styles.logo_div}>
           <Link to="/">
             <img
               className={styles.logo}
@@ -26,28 +27,52 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div>
+        <div >
           <ul className={styles.navLinks}>
             <Link to="/">
               <li>HOME</li>
             </Link>
-            <Link to="/about">
-              <li>SERVICES</li>
-            </Link>
-            <Link to="/classes">
-              <li>LOYALTY PROGRAM</li>
-            </Link>
+    
+            <li
+              onMouseEnter={() => setShowDropdown(true)}
+              onMouseLeave={() => setShowDropdown(false)}
+              className={styles.dropdown}
+            >
+              SERVICES <FaAngleDown />
+              {showDropdown && (
+                <ul className={styles.dropdownMenu}>
+                  <Link to="">
+                  <li>Car rental</li>
+                  </Link>
+                  <Link to="">
+                  <li>Trip Packages</li>
+                  </Link>
+                  <Link to="">
+                  <li>Good Shipment</li>
+                  </Link>
+                  <Link to="">
+                  <li>Parcel Pickup</li>
+                  </Link>
+                </ul>
+              )}
+            </li>
+
             <Link to="/blog&news">
               <li>BLOGS & NEWS</li>
+            </Link>
+
+            <Link to="/blog&news">
+              <li>LOYALTY PROGRAM</li>
+            </Link>
+
+            <Link to="/contactUs">
+              <li>CONTACT US</li>
             </Link>
           </ul>
         </div>
 
-        <div>
+        <div className={styles.button_div}>
           <ul className={styles.navLinks}>
-            <Link to="/contactUs">
-              <li>CONTACT US</li>
-            </Link>
             <Link to="/career">
               <li className={styles.btns}>
                 <Button
@@ -57,7 +82,6 @@ const Navbar = () => {
                 />
               </li>
             </Link>
-            {/* <Link to="/signup"> */}
             <li className={styles.btns}>
               <Button
                 bgColor={"#fff"}
@@ -78,7 +102,6 @@ const Navbar = () => {
                 }
               />
             </li>
-            {/* </Link> */}
           </ul>
         </div>
       </nav>
