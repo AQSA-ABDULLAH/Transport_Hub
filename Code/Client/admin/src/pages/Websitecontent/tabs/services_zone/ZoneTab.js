e3363bc79c55390c7c5aa9ee68c4b116f37303d2
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-29 22:23:03 +0500

    create zone tab

--- Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
+++ Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styles from "./zonetab.module.css";
 
 export default function ZoneTab() {
-  return (
-    <div>ZoneTab</div>
-  )
+    return (
+        <>
+            <div className={styles.zoneContainer}>
+                <div className={styles.avilable_zone}>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                    <p>Karachi</p>
+                </div>
+            </div>
+        </>
+    )
 }
