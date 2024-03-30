bdde39f630a922c0d49883e7bc5359c8190299db
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-30 19:59:44 +0500

    createzone header

--- Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
+++ Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from "./zonetab.module.css";
+import Button from "../../../../components/atoms/buttons/Button";
 
 export default function ZoneTab() {
     return (
         <>
             <div className={styles.zoneContainer}>
+                <div className={styles.zoneHeader}>
+                    <h2>Transport Hub Zones</h2>
+                    <Button
+                        primary
+                        btnText="Add New Zone"
+                        radius={"6px"}
+                        size={"15px"}
+                    />
+                </div>
                 <div className={styles.avilable_zone}>
-                    <p>Karachi</p>
+                    <p>Karachi <button></button></p>
                     <p>Karachi</p>
                     <p>Karachi</p>
                     <p>Karachi</p>
