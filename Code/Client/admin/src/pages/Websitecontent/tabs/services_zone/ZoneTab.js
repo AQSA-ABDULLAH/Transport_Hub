3de2e72b799b9ced7a564022c867ac5dd8475e79
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-03-30 21:19:05 +0500

    complete style

--- Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
+++ Code/Client/admin/src/pages/Websitecontent/tabs/services_zone/ZoneTab.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 import styles from "./zonetab.module.css";
 import Button from "../../../../components/atoms/buttons/Button";
 
 const ZoneTab = () => {
+    const [addZoneForm, setAddZoneForm] = useState(true);
+
     return (
         <>
             <div className={styles.zoneContainer}>
                 <div className={styles.zoneHeader}>
-                    <h2>Transport Hub Zones</h2>
-                    <Button
-                        primary
-                        btnText="Add New Zone"
-                        radius={"6px"}
-                        size={"15px"}
-                    />
-                </div>
-                <div className={styles.zone_form}>
-                    <div className={styles.form_top}>
-                        <p>Add New Zone</p>
-                        <Button
-                            secondary
-                            btnText="Cancel"
-                            radius={"6px"}
-                            size={"15px"}
-                        />
-
-                    </div>
-
-                    <form>
-                        <input type='text' placeholder='City name' />
+                    <h2>Transport Hub Zone</h2>
+                    <div onClick={() => setAddZoneForm(true)}>
                         <Button
                             primary
-                            btnText="Save Zone"
+                            btnText="Add New Zone"
                             radius={"6px"}
                             size={"15px"}
                         />
-                    </form>
+                    </div>
                 </div>
+
+                {addZoneForm &&
+                    <div className={styles.zone_form}>
+                        <div className={styles.form_top}>
+                            <p>Add New Zone</p>
+                            <div onClick={() => setAddZoneForm(false)}>
+                                <Button
+                                    secondary
+                                    btnText="Cancel"
+                                    radius={"6px"}
+                                    size={"15px"}
+                                />
+                            </div>
+                        </div>
+                        <form>
+                            <input type='text' placeholder='City name' />
+                            <Button
+                                primary
+                                btnText="Save Zone"
+                                radius={"6px"}
+                                size={"15px"}
+                            />
+                        </form>
+                    </div>
+                }
+
                 <div className={styles.avilable_zone}>
                     <p>Karachi <button><img src='/assets/images/website-content/delete.png' /></button></p>
                     <p>KarachiKarachiLkdsfn <button><img src='/assets/images/website-content/delete.png' /></button></p>
@@ -47,3 +55,4 @@ const ZoneTab = () => {
 }
 
 export default ZoneTab;
+
