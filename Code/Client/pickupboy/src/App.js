0606ac89b97eb73f031aa8d212168e462410a1a4
Fatima <fatima75.ahmad@gmail.com>
2024-04-03 09:52:03 +0500

    pickupboy dashbaord landing page

---
+++ Code/Client/pickupboy/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PickupBoyDashboard from "./pages/PickupBoyDashboard.js"
+import LoginDetails from "./pages/SidebarPages/LoginDetails.js";
+import PickupRequests from "./pages/SidebarPages/PickupRequests.js";
+import Settings from "./pages/SidebarPages/Settings.js";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/PickupBoyDashboard" element={<PickupBoyDashboard />}>
+                  <Route path="Login" element={<LoginDetails />} />
+                  <Route path="pickuprequest" element={<PickupRequests />} />
+                  <Route path="settings" element={<Settings/>} />
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ Code/Client/users/src/pages/pickupboylandingpage/PickupboysLandingPage.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import styles from "./pickupboylandingpage.module.css";
+import main from "./parcelpickupimages/main.jpeg";
+import parcelpickupimg1 from "./parcelpickupimages/parcelpickupimg1.jpeg";
+import parcelpickupimg2 from "./parcelpickupimages/parcelpickupimg2.jpeg";
+import parcelpickupimg3 from "./parcelpickupimages/parcelpickupimg3.jpeg";
+import images from "./parcelpickupimages/images.jpeg";
+// import { useNavigate } from 'react-router-dom';
+const PickupboysLandingPage = () => {
+    // const navigate = useNavigate();
+
+    // const handleRedirect = () => {
+    //   navigate('/create_shipment_form'); 
+    // };
+        
+   
+    return (
+        <>
+                <div >
+                <img className={styles.mainImage}src={main} alt="Example" />
+                </div>
+
+             <div className={styles.main}>
+                <div className={styles.container}>
+                    <section className={styles.text_area}>
+                        <h2>We will Pickup your parcel from your Door Step</h2>
+                        <p>Send your parcel from your door to any delivery service of your choice</p>
+                        {/* delivery service company images 
+                        rate calculate for sending*/}
+                       <button >Send Parcel</button>
+                    </section>
+                    <section className={styles.landing_video}>
+                    <img className={styles.side_parcel_image} src={parcelpickupimg1} alt="Example" />
+                    </section>
+                </div>
+                </div>
+                <hr></hr>
+
+
+                <div className={styles.main}>
+                <div className={styles.container}>
+                    <section className={styles.text_area}>
+                          <h2>Calculate Rate</h2>
+                           <span>
+                              <label>Pickup Location</label>
+                               <input type='text'/>
+                           </span>
+                           <span>
+                               <label>Destination</label>
+                               <input type='text'/>
+                           </span>
+                           <span>
+                               <label>Weight</label>
+                               <input type='text'/>
+                           </span>
+                           <span>
+                               <label>Weight</label>
+                               <input type='text'/>
+                           </span>
+                           <button>Calculate</button>
+                        
+                        {/* delivery service company images 
+                        rate calculate for sending*/}
+                       
+                    </section>  
+                </div>
+                </div>
+                <hr></hr>
+
+ {/* slider for images*/}
+                <div className={styles.city_lists}>
+                    <h2>You can drop your parcel at these companies</h2>
+                <div className={styles.city_list}>
+                   
+                    <div className={styles.list}>
+                    <span>
+                        <img src={parcelpickupimg1} alt="Example" />
+                        <p>Leopard</p>
+                    </span>
+                    <span>
+                        <img src={parcelpickupimg2} alt="Example" />
+                        <p>TCS</p>
+                    </span>
+                    <span>
+                        <img src={parcelpickupimg3} alt="Example" />
+                        <p>Pakistan Post</p>
+                    </span>
+                    <span>
+                        <img src={images} alt="Example" />
+                        <p>DCS</p>
+                    </span>
+                    <span>
+                        <img src={images} alt="Example" />
+                        <p>MOVEX</p>
+                    </span>
+                    <span>
+                        <img src={images} alt="Example" />
+                        <p>TCS</p>
+                    </span>
+                    </div>
+                </div>  
+                </div>
+        </>
+
+
+
+  )
+}
+
+export default PickupboysLandingPage
