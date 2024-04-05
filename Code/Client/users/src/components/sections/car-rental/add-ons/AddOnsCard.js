8088d5068b96f986962186a79bdadc69f8762967
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-04-06 03:53:37 +0500

    style add on card

--- Code/Client/users/src/components/sections/car-rental/add-ons/AddOnsCard.js
+++ Code/Client/users/src/components/sections/car-rental/add-ons/AddOnsCard.js
@@ -6,16 +6,26 @@ export default function AddOnsCard() {
     <>
       <div className={style.Container}>
         <div className={style.heading}>
-          <h2 className={style.h2}>Select Upgrades and AddsOn for Your Trip</h2>
-          <p className={style.p}>Enhance Your Car</p>
+          <h2>Select Upgrades and AddsOn for Your Trip</h2>
+          <p>Enhance Your Car</p>
         </div>
         <div className={style.cards}>
           <div className={style.addOnCard}>
             <img src="" alt="icon"/>
             <div>
-              <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-              <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
+              <h3>Car driver</h3>
+              <span>Need a driver to drive with car.</span>
             </div>
+            <div className={style.price}>
+              <p>RS 450 <span>Per Day</span></p>
+              <button>Add</button>
+            </div>
+          </div>
+
+          <div className={style.addOnCard}>
+            <img src="" alt="icon" />
+            <h3>Car Seat (Infant)</h3>
+            <span>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</span>
             <div className={style.price}>
               <p>RS 320 <span>Per Day</span></p>
               <button>Add</button>
@@ -24,58 +34,16 @@ export default function AddOnsCard() {
 
           <div className={style.addOnCard}>
             <img src="" alt="icon" />
-            <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-            <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
+            <h3>Car Seat (Toddler)</h3>
+            <span>Choose an toddler seat for a safer trip. Suitable for child up to 1 to 3 years old.</span>
             <div className={style.price}>
-              <p>RS 320 <span>Per Day</span></p>
-              <button>Add</button>
-            </div>
-          </div>
-
-          <div className={style.addOnCard}>
-            <img src="" alt="icon" />
-            <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-            <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
-            <div className={style.price}>
-              <p>RS 320 <span>Per Day</span></p>
+              <p>RS 300 <span>Per Day</span></p>
               <button>Add</button>
             </div>
           </div>
         </div>
 
-        <div className={style.cards}>
-          <div className={style.addOnCard}>
-            <img src="" alt="icon"/>
-            <div>
-              <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-              <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
-            </div>
-            <div className={style.price}>
-              <p>RS 320 <span>Per Day</span></p>
-              <button>Add</button>
-            </div>
-          </div>
-
-          <div className={style.addOnCard}>
-            <img src="" alt="icon" />
-            <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-            <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
-            <div className={style.price}>
-              <p>RS 320 <span>Per Day</span></p>
-              <button>Add</button>
-            </div>
-          </div>
-
-          <div className={style.addOnCard}>
-            <img src="" alt="icon" />
-            <h3 className={style.cardTitle}>Car Seat (Infant)</h3>
-            <p>Choose an infant seat for a safer trip. Suitable for babies up to 12 months old.</p>
-            <div className={style.price}>
-              <p>RS 320 <span>Per Day</span></p>
-              <button>Add</button>
-            </div>
-          </div>
-        </div>
+    
       </div>
     </>
   )
