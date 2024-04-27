e23ebb3180cfabe4e844e583faa9a66aed9807e5
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-04-28 00:16:42 +0500

    create terms and condition page

--- Code/Client/users/src/pages/career/driver/terms&condition/Terms&Condition.js
+++ Code/Client/users/src/pages/career/driver/terms&condition/Terms&Condition.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import careerStyles from '../../careerpage.module.css';
+import styles from './terms&condition.module.css'
 import MediumHeader from '../../../../components/sections/header-medium/MediumHeader';
+import { Link } from 'react-router-dom';
 
 export default function TermsCondition() {
     return (
@@ -8,8 +10,23 @@ export default function TermsCondition() {
             <div className={careerStyles.container}>
                 <div className={careerStyles.subContainer}>
                     <MediumHeader />
-                    <div>
-                        ghjgj
+                    <div className={styles.application_container}>
+                        <div className={styles.welcome_section}>
+                            <h1>Terms and Conditions</h1>
+                            <p>Please review and agree to the documents below.</p>
+                        </div>
+
+                        <div className={styles.terms_condition}>
+                            <Link to={"/driver_terms_and_conditions"}><h4>Subscription Pass Terms and Conditions</h4></Link>
+                            <hr />
+                            <Link to={"/driver_terms_and_conditions"}><h4>Services Agreement</h4></Link>
+                            <hr />
+                        </div>
+
+                        <div className={styles.welcome_section}>
+                            <button>Submit Application</button>
+                        </div>
+
                     </div>
                 </div>
             </div>
