79bc705d5475a722d0deea7a19f051172a347293
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-04-28 21:06:00 +0500

    create driver Model

--- Code/Server/models/Drivers.js
+++ Code/Server/models/Drivers.js
@@ -4,6 +4,16 @@ const jwt = require('jsonwebtoken')
 const driversSchema = new mongoose.Schema({
     driverEmail: { type: String, unique:true, trim: true },
     password: { type: String, required: true, trim: true },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    driver_location:{ type: String, required: true, trim: true },
+    vechicalType:{ type: String, required: true, trim: true },
+    termsAndCondition:{ type: Boolean, required: true, trim: true },
+    profilePhoto:{ type: String, required: true, trim: true },
+    cnicFrontSide:{ type: String, required: true, trim: true },
+    cnicBackSide:{ type: String, required: true, trim: true },
+    drivingLicense:{ type: String, required: true, trim: true },
+    status:{ type: Boolean, default:false, trim: true },
     otp: { type:String, required:true, trim: true },
     is_verified: { type: Boolean, default: false },
     tokens: [
