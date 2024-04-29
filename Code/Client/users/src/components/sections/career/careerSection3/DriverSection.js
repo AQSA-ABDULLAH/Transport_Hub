f73258221a95e50a15f95b1348e7cf7d3d39c920
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-04-29 05:33:56 +0500

    create career page

--- Code/Client/users/src/components/sections/career/careerSection3/DriverSection.js
+++ Code/Client/users/src/components/sections/career/careerSection3/DriverSection.js
@@ -1,21 +1,31 @@
-// DriverSection.jsx
-
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Assuming you are using React Router for navigation
+import { useNavigate } from 'react-router-dom';
+import style from "./driverSection.module.css"
 
 export default function DriverSection() {
     const navigate = useNavigate();
 
     const handleRedirect = () => {
-        navigate('/driverRegistration'); // Replace '/RentalBookingPage' with the actual path to your driver login page
+        navigate('/driverRegistration');
     };
 
     return (
-        <div>
-            <h2>Driver Section</h2>
-            <p>This is the driver section content.</p>
-            <button onClick={handleRedirect}>Become a Driver</button>
-        </div>
+        <>
+            <div className={style.container_heading}>
+                <h1>How do we reimagine & improve
+                    lives of our truck drivers.</h1>
+            </div>
+            <div className={style.career_container}>
+                <div className={style.image_section}>
+                    <img src='./assets/images/career/driver.jpg' />
+                </div>
+                <div className={style.content_section}>
+                    <h2>We help Drivers with:</h2>
+                    <p>This is the driver section content.</p>
+                    <button onClick={handleRedirect}>Become a Driver</button>
+                </div>
+            </div>
+        </>
     );
 }
 
