496cba2244ba56dacae85bce8be13b7db6d8da16
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-05-12 04:54:02 +0500

    style rental view

--- Code/Client/users/src/pages/manage_booking/rental_reservation/ViewRental.js
+++ Code/Client/users/src/pages/manage_booking/rental_reservation/ViewRental.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import style from "./managerental.module.css";
+import axios from 'axios';
 import Sidebar from '../../../components/molecules/manage-booking/sidebar/Sidebar';
 import TopHeader from '../../../components/molecules/manage-booking/booking-header/TopHeader';
 
 function ViewRental() {
     const [openSidebar, setOpenSidebar] = useState(false);
     const [rentalData, setRentalData] = useState(null);
+    const [product, setProduct] = useState([]);
 
-
-    // GET DATA FROM LOCALSTORAGE
     useEffect(() => {
         const storedRentalData = localStorage.getItem('rentalData');
         if (storedRentalData) {
             const parsedRentalData = JSON.parse(storedRentalData);
             setRentalData(parsedRentalData);
+            const car_id = parsedRentalData.car_id;
+            axios.get(`http://localhost:5000/api/cars/getCar/${car_id}`)
+                .then(res => {
+                    console.log(res.data);
+                    setProduct(res.data.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }, []);
-    const _id = rentalData ? rentalData._id : null;
+
 
     const handleCancelReservation = () => {
         if (rentalData) {
@@ -40,13 +49,67 @@ function ViewRental() {
                     <h2>Your Rental Details</h2>
                     <button onClick={handleCancelReservation}>Cancel Reservation</button>
                 </div>
-                <h4>Confirmation: {_id}</h4>
-                <div>
-                    <h3>Vehicle</h3>
-                    <p>First Name: {rentalData?.firstName}</p>
-                    <p>Last Name: {rentalData?.lastName}</p>
-                    <p>Email: {rentalData?.email}</p>
-                </div>
+                <h4>Confirmation: {rentalData ? rentalData._id : null}</h4>
+                <section>
+                    <div className={style.container}>
+                        <div className={style.addOnContainer}>
+                            <div>
+                                <h1>Vehicle Details :</h1>
+                                <div className={style.car_header}>
+                                    <div>
+                                        <img src="./assets/images/cars/car4.png" alt="car" />
+                                    </div>
+                                    <div>
+                                        <h2>GAC GN8 7-SEATS MPV MODEL 2023</h2>
+                                        <p>civic</p>
+                                    </div>
+                                </div>
+
+                                <h1>Itinerary :</h1>
+
+                                <div className={style.booking_time}>
+                                    <div>
+                                        <span>{rentalData?.pickupDate}</span> -
+                                        <span> {rentalData?.pickupTime}</span>
+                                        <p>{rentalData?.pickupLocation}</p>
+                                    </div>
+                                    <div>
+                                        <span>{rentalData?.dropDate}</span> -
+                                        <span> {rentalData?.dropTime}</span>
+                                        <p>{rentalData?.dropLocation}</p>
+                                    </div>
+                                </div>
+
+                                <div className={style.extras}>
+                                    <h1>Extras :</h1>
+                                    <p>Loss Damage Waiver</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.priceContainer}>
+                            <div className={style.price_card_contanier}>
+                                <h3>Price Summary</h3>
+                                <div className={style.price}>
+                                    <p>Base Rate</p>
+                                    <p>$54.95</p>
+                                </div>
+                                <div className={style.price}>
+                                    <p>Add On Price</p>
+                                    <p>$54.95</p>
+                                </div>
+                                <div className={style.price}>
+                                    <p>Taxes and Fees</p>
+                                    <p>Free</p>
+                                </div>
+                                <div className={`${style.price} ${style.price_per_day}`}>
+                                    <p>Price for 1 day</p>
+                                    <p>$111.54</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </section>
         </>
     );
@@ -55,3 +118,18 @@ function ViewRental() {
 export default ViewRental;
 
 
+
+
+
+
+
+
+
+
+
+// {Array.isArray(product) && product.map((item, index) => (
+//     <div key={index}>
+//         <img src={item?.carImage} alt="car" />
+//         <h2>{item.carTitle}</h2>
+//     </div>
+// ))}
